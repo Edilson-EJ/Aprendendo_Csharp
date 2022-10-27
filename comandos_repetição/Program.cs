906b529329e787d109e04895de6999efdfeb7757
Edilson-EJ <edilsonjunior624@gmail.com>
2022-10-26 22:16:28 -0300

    apredendo c#

---
+++ comandos_repetição/Program.cs
@@ -0,0 +1,25 @@
+﻿Boolean condicao = true;
+
+int valor =5;
+int i = 0;
+
+while(valor >= i){
+    Console.WriteLine("while!!" + i);
+    i++;
+}
+
+i = 0;
+do{
+    Console.WriteLine(" do while!!" + i);
+    i++;
+}while(valor >= i);
+
+for( int j = 0; valor >= j; j++){
+    Console.WriteLine("for: " + j);
+}
+
+int[] lista = {1,2,3,4,5};
+
+foreach(int numero in lista){
+    Console.WriteLine("ForEach: " + numero);
+}
